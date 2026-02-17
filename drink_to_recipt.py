6d1a00b3ddf113f2cc5b1cb77b6411eb8817fb67
matthew_hebert <matthew_hebert@tamu.edu>
2026-02-17 00:01:04 -0600

    Created code to fill in table for drink to receipt. 50% chance 1 drink per receipt, %25 two, %25 three

--- drink_to_recipt.py
+++ drink_to_recipt.py
@@ -3,3 +3,28 @@ This is dedicated to the Most Sacred Heart of Jesus,
 the Immaculate Heart of Mary 
 and the Chaste Heart of St. Joseph
 """
+import random
+
+header = "drink_id, receipt_id\n"
+
+#50% chance one drink ordered per receipt
+#50% chance two drinks. If two drinks ordered, 50% chance an additional third drink ordered
+
+with open("drink_to_recipt.csv", "w") as file:
+    file.write(header)
+
+    for i in range(0,100):
+        rowString = f"{i % 15}, {i}\n"
+        file.write(rowString)
+
+        if random.randint(0,1) == 0:
+            rowString = f"{(i + 10) % 15}, {i}\n"
+            file.write(rowString)
+
+            if random.randint(0,1) == 0:
+                rowString = f"{(i + 20) % 15}, {i}\n"
+                file.write(rowString)
+
+        
+
+        
