e3a7546a6cfb2e067338cd337b6557cd9939ab3e
Julia <jlstreet@tamu.edu>
2026-02-18 18:48:57 -0600

    added comments

--- food.py
+++ food.py
@@ -1,10 +1,11 @@
 
+# not many food options so randomization not needed
 foodVaraieties = [
     {"item": "BBQ pork", "price": 9.99},
     {"item": "Teriyaki chicken", "price": 9.99},
     {"item": "French fries", "price": 3.50}
 ]
-with open("food.csv", "w") as file:
+with open("food.csv", "w") as file:  # create food datbase
     file.write("id, name, price\n")
     for id, food in enumerate(foodVaraieties, start=1):
         file.write(f"\n{id},{food['item']},{food['price']}")
